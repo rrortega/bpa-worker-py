fbaa7cb789eda000c2bbf128e78dc4f1c4388405
rrortega <rolymayo11@gmail.com>
2021-10-13 14:09:51 -0500

    prepare enviromoent

---
+++ bpa.py
@@ -0,0 +1,157 @@
+import base64
+import warnings
+import requests
+import bs4
+import json
+import os
+# pip install pysocks
+# pip3 install --user 'requests[socks]'
+class BpaCrawler:
+    def __init__(self, params, proxies):
+        super().__init__()
+        self.ssn = requests.Session()
+        self.logged_in = False
+        self.data = {
+            'username': params['passw'],
+            'password': params['pin']
+        }
+        self.pin = params['pin']
+        self.account = params['card_no']
+        self.matrix = params
+
+        if proxies != None:
+            self.ssn.proxies = proxies
+
+    def health_check(self):
+        warnings.filterwarnings("ignore")
+        try:
+            check = self.ssn.get('https://bancaremota.bpa.cu/',  timeout=(5,10), verify=False)
+#             print(check.content.strip().find("no se encuentra disponible"))
+            return {"status":"online"} if check.status_code == 200    else {"status":"offline"}
+        except:
+            return {"status":"offline"}
+        return {"status":"offline"}
+    def login(self):
+        warnings.filterwarnings("ignore")
+        loginResponse = self.ssn.post('https://bancaremota.bpa.cu/ajax.php?opp=login', data=self.data,timeout=(3,5), verify=False)
+        if loginResponse.status_code == 200 and '' != loginResponse.content:
+             try:
+                r=json.loads(loginResponse.content)
+                m1=self.matrix[r['MATRIZ'][0]]
+                m2=self.matrix[r['MATRIZ'][1]]
+                mfaResponse = self.ssn.post('https://bancaremota.bpa.cu/ajax.php?opp=validate', data={'matrix-1':m1,'matrix-2':m2}, verify=False)
+                self.logged_in =  200 == mfaResponse.status_code
+             except: #se partio por algo
+                 return False
+        return False
+    def holder_lockup(self, account):
+        if not self.logged_in:
+            self.login()
+        if not self.logged_in:
+           return {  'msg': 'No fue posible conectar a BPA con estas credenciales' }
+
+        resp = self.ssn.post('https://bancaremota.bpa.cu/ajax.php?opp=titular_pan', data={'pan': account}, verify=False)
+        if resp.status_code == 200 :
+           r=json.loads(resp.content)
+           try:
+               return r['CLIENTE']
+           except IndexError:
+                return 'UNKNOWN'
+
+        return 'UNKNOWN'
+
+    def get_accounts(self):
+        if not self.logged_in:
+            self.login()
+        if not self.logged_in:
+           return {  'msg': 'No fue posible conectar a BPA con estas credenciales' }
+
+        accounts = []
+        resp = self.ssn.post('https://bancaremota.bpa.cu/ajax.php?opp=ultimos_movimientos', data={'cuenta':self.account}, verify=False)
+        if resp.status_code == 200 :
+           r=json.loads(resp.content)
+           balances = dict()
+           currency = 'CUP'
+           if self.account[:4] == '9225':
+              currency = 'USD'
+           if self.account[:4] == '9200':
+              currency = 'CUC'
+           trs = r['OPERACIONES']
+           t = len(r['OPERACIONES'])
+           account = {
+              "number" : self.account,
+              "currency": currency,
+              "balance": 0 if t == 0 else float(r['OPERACIONES'][t-1]['Saldo'].replace(' ','')),
+              "transactions": []
+           }
+           for t in trs:
+               amount = t['Imp_Asient'].replace(' ','')
+               account["transactions"].append({
+                   'date': t['Fec_Contab'],
+                   'ref': t['Ref_Origin'],
+                   'amount': float(amount) * (-1 if t['Tipo_Op'] == 'DB' else 1),
+                   'current_balance': float(t['Saldo'].replace(' ','')),
+                   'memo': t['Observ']
+               })
+
+           accounts.append(account)
+
+        return accounts
+    def update_limits(self, amount):
+        if not self.logged_in:
+            self.login()
+        if not self.logged_in:
+            return {  'msg': 'No fue posible conectar a BPA con estas credenciales' }
+
+        payload={'cuenta':self.account, 'importe_diario_atm': amount, 'importe_diario_pos': amount, 'importe_total_diario': amount}
+        resp = self.ssn.post('https://bancaremota.bpa.cu//ajax.php?opp=cambiar_limites', data=payload, verify=False)
+
+        if resp.status_code == 200 :
+           r=json.loads(resp.content)
+           return r['message']
+        else :
+            r=json.loads(resp.content)
+            if None != r and None != r['ERROR']:
+                return {  'msg': r['ERROR'] }
+        return {  'msg': 'Error actualizando limites'   }
+
+if __name__ == '__main__':
+    token=os.environ['JSON_B64'].strip()
+    params = json.loads(str(base64.b64decode(token), "utf-8").replace('_NH_','Ñ'))
+#     proxies={
+#          'http': 'socks5://localhost:1040',
+#          'https': 'socks5://localhost:1040',
+#     }
+    bc = BpaCrawler( params, None )
+
+    action = 'sync'
+    if os.environ.get('ACTION'):
+        action = os.environ['ACTION'].strip()
+
+
+    if action == 'healthcheck' or action == 'health_check' or action == 'ping':
+        print(bc.health_check())
+
+    if action == 'lockup':
+        print(bc.holder_lockup(os.environ['account'].strip()))
+
+
+    if action == 'sync':
+        print(bc.get_accounts())
+
+    if action == 'upgrade_limits':
+        amount = os.environ['AMOUNT'].strip()
+        limits = json.dumps(bc.update_limits(amount))
+        print(limits)
+
+#     if action == 'upgrade_limits':
+#         amount = os.environ['AMOUNT'].strip()
+#         limits = json.dumps(bc.update_limits(amount))
+#         print(limits)
+#     if action == 'transfer':
+#         account = os.environ['ACCOUNT'].strip()
+#         amount = os.environ['AMOUNT'].strip()
+#         response=json.dumps(bc.transfer(account, amount))
+#         print(response)
+
+#
