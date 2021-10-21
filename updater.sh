02ece8991ed17c5ca5ba80a162fb2a1707e1e806
rrortega <rolymayo11@gmail.com>
2021-10-21 12:05:03 -0500

    updater and run checks

--- updater.sh
+++ updater.sh
@@ -1,4 +1,4 @@
-                          #!/bin/sh
+#!/bin/sh
 
 # Take from https://stackoverflow.com/a/3278427/3384529
 git fetch
@@ -8,12 +8,21 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
+#checar si hay cambios y actualizar codigo
 if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
+    echo "!!!EL REPO ESTA AL DIA!!!"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+    echo "SE HAN DETECTADO NUEVOS CAMBIOS!"
+    echo "> Deteniendo el container..."
+    echo "> Actualizando cambios..."
+    git pull
+    docker-compose down
+    echo "!!!CODIGO ACTUALIZADO!!!"
 elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
-else
-    echo "Diverged"
+    echo "Has modificado codigo en local, comunicate con el administrador del repo o crea un fork para que puedas subir tus cambios"
 fi
+
+
+#mandar a correr
+echo "> Mandar a correr el container..."
+sh ./run.sh
