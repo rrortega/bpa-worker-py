174d2a0299fa6e6b511c16165a9a6cc50625ef27
rrortega <rolymayo11@gmail.com>
2021-10-21 12:05:53 -0500

    updater and run checks

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ if [[ $STATUS =~ "Exited" ]]; then
   echo "> Despertndo el contenedor..."
   docker-compose up -d
 else
-  echo "El container ya estaba corriendo!"
+  echo "El container esta corriendo!"
 fi
