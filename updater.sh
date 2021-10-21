ef96115cb44e2d4e3fee61d1db2284235f7ecd66
rrortega <rolymayo11@gmail.com>
2021-10-21 11:35:06 -0500

    updater.sh

---
+++ updater.sh
@@ -0,0 +1,19 @@
+                          #!/bin/sh
+
+# Take from https://stackoverflow.com/a/3278427/3384529
+git fetch
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
